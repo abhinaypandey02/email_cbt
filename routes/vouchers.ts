39a8edd78538c43a76850bab29d644c12e16c98f
Abhinay Pandey <abhinaypandey@uber.com>
2024-08-05 21:42:08 +0530

    voucher route

--- routes/vouchers.ts
+++ routes/vouchers.ts
@@ -2,6 +2,6 @@ import {RequestHandler} from "express";
 import axios from "axios";
 
 export const handleVouchers:RequestHandler=async (req,res)=>{
-    const {data} = await axios.get(process.env.GATSBY_STRAPI_LOCAL_ENDPOINT+':1337/api/voucher');
+    const {data} = await axios.get(process.env.GATSBY_STRAPI_LOCAL_ENDPOINT+':1337/api/coupons');
     return res.status(200).send(data)
 }
