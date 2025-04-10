bed419674ab4baf7b8ee45280d298d8fafc6dac8
Abhinay Pandey <abhinay.pandey@springy.se>
2025-04-10 18:24:49 +0530

    fixes

--- client.ts
+++ client.ts
@@ -4,10 +4,10 @@ const client = new Whatsapp.Client({
     authStrategy: new Whatsapp.LocalAuth()
 });
 
-client.on('message', msg => {
-    msg.
+client.on('message',async msg => {
+    console.log(msg.from)
     if (msg.body.includes('form')&&msg.body.includes('https://')&&!msg.fromMe) {
-        fetch('https://sociocube.com/api/handle-whatsapp',{
+        const data = await fetch('https://sociocube.com/api/handle-whatsapp',{
             method: 'POST',
             body: JSON.stringify({
                 body: msg.body,
@@ -15,6 +15,8 @@ client.on('message', msg => {
                 chat: msg.from
             }),
         })
+        const res = await data.text()
+
     }
 });
 client.initialize()
