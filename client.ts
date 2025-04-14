f0e819fdc82130108dcb92924b27c2c066adc02d
Abhinay Pandey <abhinay.pandey@springy.se>
2025-04-14 16:04:34 +0530

    fixes

--- client.ts
+++ client.ts
@@ -1,22 +1,28 @@
 import Whatsapp from 'whatsapp-web.js'
 
+const queue = []
+
 const client = new Whatsapp.Client({
     authStrategy: new Whatsapp.LocalAuth()
 });
 
 client.on('message',async msg => {
     if (msg.body.includes('form')&&msg.body.includes('https://')&&!msg.fromMe) {
-        const data = await fetch('https://sociocube.com/api/handle-whatsapp',{
-            method: 'POST',
-            body: JSON.stringify({
-                body: msg.body,
-                author: msg.author,
-                chat: msg.from
-            }),
-        })
-        const res = await data.text()
-        if(res) await client.sendMessage("919811954465@c.us",res)
-
+        queue.push(msg);
+        const i = queue.length;
+        setTimeout(async ()=>{
+            const data = await fetch('https://sociocube.com/api/handle-whatsapp',{
+                method: 'POST',
+                body: JSON.stringify({
+                    body: msg.body,
+                    author: msg.author,
+                    chat: msg.from
+                }),
+            })
+            const res = await data.text()
+            if(res) await client.sendMessage("919811954465@c.us",res)
+            queue.splice(i, 1);
+        },(queue.length+1)*5000);
     }
 });
 client.initialize()
