44d53d1f7cbb3367e0b010c3b7a504e8886f93f6
Abhinay Pandey <abhinay.pandey@springy.se>
2025-07-12 14:50:51 +0530

    update

--- client.ts
+++ client.ts
@@ -2,7 +2,6 @@ import Whatsapp, {Channel} from 'whatsapp-web.js'
 import qrcode from 'qrcode-terminal';
 
 const queue = []
-const threads:string[] = []
 
 const client = new Whatsapp.Client({
     authStrategy: new Whatsapp.LocalAuth()
@@ -12,52 +11,29 @@ let IndiaChannel:Channel, GlobalChannel:Channel;
 
 setInterval(async () => {
     const msg = queue.pop()
-    if(msg){
-        const data = await fetch('https://sociocube.com/api/handle-whatsapp',{
-            method: 'POST',
-            body: JSON.stringify({
-                body: msg.body,
-                author: msg.author,
-                chat: msg.from
-            }),
-        })
-        const res = await data.text()
+    const data = await fetch('https://sociocube.com/api/handle-whatsapp',{
+        method: 'POST',
+        body: msg || "{}",
+    })
+    const { nextPost, error } = await data.json()
 
-        if(data.status===200) {
-            threads.push(res)
-            await IndiaChannel.sendMessage(res)
-            if(!res.includes('India')){
-                await GlobalChannel.sendMessage(res)
-            }
-        } else {
-            console.error(res)
+    if(data.status===200) {
+        await IndiaChannel.sendMessage(nextPost)
+        if(!nextPost.includes('India')){
+            await GlobalChannel.sendMessage(nextPost)
         }
+    } else {
+        console.error(error)
     }
-},30000)
+},60000*2)
 
-setInterval(async () => {
-    const msg = threads.pop()
-    if(msg){
-        const data = await fetch('https://sociocube.com/api/handle-threads',{
-            method: 'POST',
-            body:JSON.stringify({
-                thread:[
-                    [...msg.split('\n').slice(0,-1), 'Apply now from pinned comment 👇'].join('\n'),
-                    msg.split('\n').at(-1),
-                ],
-                token: "dsfjbn13r4tdnlnjkfj1243vdfbjkn123r4ebfjsdfvef"
-            }),
-        })
-        if(data.status===501) {
-            const res = await data.text()
-            console.error(res)
-        }
-    }
+setInterval(() => {
+    fetch('https://sociocube.com/api/handle-threads')
 },60000*60)
 
 client.on('message',async msg => {
     if (msg.body.includes('forms')&&msg.body.includes('https://')&&!msg.fromMe) {
-        queue.push(msg);
+        queue.push(msg.body);
     }
 });
 
